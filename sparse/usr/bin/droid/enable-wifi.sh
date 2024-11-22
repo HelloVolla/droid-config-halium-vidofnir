a2ff314e98b4bc74f07edea07c4611718484ca6e
Adam Pigg <adam@piggz.co.uk>
2024-11-22 21:22:39 +0000

    Align wlan script with ubports

--- sparse/usr/bin/droid/enable-wifi.sh
+++ sparse/usr/bin/droid/enable-wifi.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-#while [ ! -e "/dev/wmtWifi" ]; do
-while [ "`getprop vendor.connsys.driver.ready`" != "yes" ]; do
-        sleep 1
-done
+# Wait for the property system to be up.
+while [ ! -e /dev/socket/property_service ]; do sleep 0.1; done
 
-sleep 3
+# Wait for nvram to be loaded.
+while [ "$(getprop vendor.service.nvram_init)" != "Ready" ]; do sleep 0.2; done
+while [ "$(getprop vendor.mtk.nvram.ready)" != "1" ]; do sleep 0.2; done
 
-echo 1 > /dev/wmtWifi
+# enable Wi-Fi adapter in client mode
+while [ ! -e /dev/wmtWifi ]; do sleep 0.2; done
+echo P > /dev/wmtWifi
+
+# set wifi.interface property since urfkill.conf expects that
+setprop wifi.interface wlan0
