d231b056060255ed2f0cf8ec9a225e8435f68c7d
Adam Pigg <adam@piggz.co.uk>
2025-11-01 16:40:49 +0000

    Update setup-configfs to lastest style script

--- sparse/usr/bin/droid/setup-configfs.sh
+++ sparse/usr/bin/droid/setup-configfs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #Set up configfs as we are not using droid-boot init-script
-
-USB_FUNCTIONS=ecm,mtp
+USB_DRIVER=ecm
+USB_FUNCTIONS=$USB_DRIVER,mtp
 GADGET_DIR=/sys/kernel/config/usb_gadget
 
 # Sugar for accessing usb config
@@ -18,19 +18,19 @@ write $GADGET_DIR/g1/strings/0x409/serialnumber "$1"
 write $GADGET_DIR/g1/strings/0x409/manufacturer "Halium"
 write $GADGET_DIR/g1/strings/0x409/product      "Halium Device"
 
-if echo $USB_FUNCTIONS | grep -q "ecm"; then
-    mkdir $GADGET_DIR/g1/functions/ecm.usb0
-fi
+echo $USB_FUNCTIONS | grep -q "$USB_DRIVER"  && mkdir $GADGET_DIR/g1/functions/$USB_DRIVER.usb0
 echo $USB_FUNCTIONS | grep -q "mass_storage" && mkdir $GADGET_DIR/g1/functions/storage.0
-echo $USB_FUNCTIONS | grep -q "mtp" && mkdir $GADGET_DIR/g1/functions/ffs.mtp
+echo $USB_FUNCTIONS | grep -q "mtp"          && mkdir $GADGET_DIR/g1/functions/ffs.mtp
 
 mkdir $GADGET_DIR/g1/configs/b.1
 mkdir $GADGET_DIR/g1/configs/b.1/strings/0x409
 write $GADGET_DIR/g1/configs/b.1/strings/0x409/configuration "$USB_FUNCTIONS"
 
-if echo $USB_FUNCTIONS | grep -q "ecm"; then
-    ln -s $GADGET_DIR/g1/functions/ecm.usb0 $GADGET_DIR/g1/configs/b.1
-fi
+echo $USB_FUNCTIONS | grep -q "$USB_DRIVER"  && ln -s $GADGET_DIR/g1/functions/$USB_DRVER.usb0 $GADGET_DIR/g1/configs/b.1
 echo $USB_FUNCTIONS | grep -q "mass_storage" && ln -s $GADGET_DIR/g1/functions/storage.0 $GADGET_DIR/g1/configs/b.1
+echo $USB_FUNCTIONS | grep -q "mtp"          && ln -s $GADGET_DIR/g1/functions/ffs.mtp $GADGET_DIR/g1/configs/b.1
 
-echo "$(ls /sys/class/udc/m*)" > $GADGET_DIR/g1/UDC
+echo "" > $GADGET_DIR/g1/UDC
+echo "$(ls /sys/class/udc | grep -v dummy | head -n 1)" > $GADGET_DIR/g1/UDC
+
+exit 0
